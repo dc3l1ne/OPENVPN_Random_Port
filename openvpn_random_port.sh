9679edac637ec87cc0f0653ad490e4ec90b9b5c5
Dc3 <admin@dc3.es>
2015-10-22 10:42:23 +0800

    Update openvpn_random_port.sh

--- openvpn_random_port.sh
+++ openvpn_random_port.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ###
-ip=133.130.103.118
+ip=
 sshuser=root
 sshport=122
 serverip=192.168.2.1
 clientip=192.168.2.2
 keyfile=s.key
-salt=x3KV8lBxynZZ5C2cUmZZpDIgjJ0x2BclvvAZkgBKHn7lB1joDGcZg8d8B1xmVySt
+salt=x3KnZV8lBxynZZ5C2cU
 ###
 function random_port_generator (){
 while true
@@ -50,21 +50,33 @@ echo "Port change to:$rad"
 echo "Port change to:$rad" >> /mnt/vpnlogs
 }
 function restart_vpn_server (){
+			echo Uploading file....
 			scp -P $sshport server.conf $sshuser@$ip:/etc/openvpn
 			ssh -p $sshport $sshuser@$ip "killall openvpn"
 			ssh -p $sshport $sshuser@$ip "openvpn --cd /etc/openvpn --config server.conf >> /dev/null &"
 			date=`date "+%Y-%m-%d %T"`
-			echo "VPN down!Server Restarting.....  $date" >> /mnt/vpnlogs
-			echo "VPN down!Server Restarting.....  $date"
+			echo "Server Restarting.....  $date" >> /mnt/vpnlogs
+			echo "Server Restarting.....  $date"
 }
 function restart_vpn_client (){
 			killall openvpn
 			cp client.conf /etc/openvpn
 			openvpn --cd /etc/openvpn --config client.conf >> /dev/null &
 			date=`date "+%Y-%m-%d %T"`
-			echo "VPN down!Client Restarting.....  $date" >> /mnt/vpnlogs
-			echo "VPN down!Client Restarting.....  $date"
+			echo "Client Restarting.....  $date" >> /mnt/vpnlogs
+			echo "Client Restarting.....  $date"
 }
+if [ $# = 1 ]
+then
+	if [ $1 = 999 ]
+		then
+			echo Generating new port...
+			random_port_generator
+			restart_vpn_server
+			sleep 1
+			restart_vpn_client
+	fi
+fi
 vpn_status=`ifconfig |grep $clientip`
 if [ -z "$vpn_status" ]
 then
@@ -94,6 +106,7 @@ do
 			restart_vpn_client
 			sleep 5
 			ping=`ping -c 5 $serverip |grep received |cut -b 24`
+			echo $ping
 			if [ $ping -eq 0 ]
 			then
 				sleep 5
@@ -110,6 +123,7 @@ do
 		then
 			sleep 5
 		else
+			echo $ping
 			date=`date "+%Y-%m-%d %T"`
 			echo "VPN Restarted! $date" >> /mnt/vpnlogs
 			echo "VPN Restarted! $date"
